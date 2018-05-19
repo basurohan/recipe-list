ede42f6a7f541a60d984d87d608c3ee1e2e26114
“Rohan <Rohan@CH15-82.local>
2018-05-19 00:39:39 -0400

    Added routes

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { Recipe } from '../../recipe.model';
 import { RecipeService } from '../../recipe.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-recipe-item',
@@ -10,14 +12,16 @@ import { RecipeService } from '../../recipe.service';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
+  @Input() index: number;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService, private router: Router) { }
 
   ngOnInit() {
   }
 
   onSelected() {
-    this.recipeService.recipeSelected.emit(this.recipe);    
+    // this.recipeService.recipeSelected.emit(this.recipe);  
+    // this.router.navigate(['recipes', this.index]);
   }
 
 }
---
+++ src/app/recipes/recipes-start/recipes-start.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-start',
+  templateUrl: './recipes-start.component.html',
+  styleUrls: ['./recipes-start.component.css']
+})
+export class RecipesStartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
